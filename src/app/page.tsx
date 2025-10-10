a3df23dd72d3114421b963f30eca061d30a2cc37
Nindy <macbookpro@nindyeriyani.local>
2025-10-10 21:50:32 +0800

    feat: add About Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "@/components/layout/Navbar";
 import Hero from "@/components/layout/Hero";
+import AboutSection from "@/components/sections/AboutSection";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <AboutSection />
     </main>
   );
 }
---
+++ src/components/sections/AboutSection.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Image from "next/image";
+
+export default function AboutSection() {
+  return (
+    <section className="bg-[#EEF2F6] min-h-screen flex flex-col justify-center items-center px-6 md:px-16 text-center text-gray-800">
+      {/* Top section: h2 and logo */}
+      <div className="mb-12">
+        <h2 className="text-2xl md:text-3xl font-semibold mb-8 text-gray-800">
+          Tentang
+        </h2>
+        <div className="flex items-center justify-center gap-3">
+          <Image
+            src="/logo-gdgoc-unm-b.png"
+            alt="GDG On Campus Logo"
+            width={400}
+            height={60}
+            className="mx-auto"
+          />
+        </div>
+      </div>
+
+      {/* Middle section: paragraph centered */}
+      <div className="max-w-4xl mx-auto text-base md:text-xl leading-loose text-gray-700 mt-6">
+        <p>
+          <strong>
+            Google Developer Group On Campus (GDSC) Universitas Negeri Makassar
+          </strong>{" "}
+          adalah komunitas mahasiswa yang berfokus pada pengembangan teknologi
+          Google. Kami terbuka untuk <strong>seluruh mahasiswa</strong> dari
+          berbagai jenjang yang ingin{" "}
+          <strong>
+            belajar, berkolaborasi, dan berkembang sebagai developer
+          </strong>
+          . Melalui pembelajaran berbasis <strong>peer-to-peer</strong>, anggota
+          GDSC UNM berkesempatan untuk membangun keterampilan praktis,
+          mengembangkan proyek nyata, dan menjadi bagian dari{" "}
+          <strong>komunitas global Google Developer</strong>.
+        </p>
+      </div>
+    </section>
+  );
+}
